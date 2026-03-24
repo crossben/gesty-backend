95faa41fc224b863dac67ced76607c4b37973302
crossben <gueco167@gmail.com>
2026-03-24 23:48:24 +0000

    feat: Add `is_active` column to the `schools` table.

--- database/migrations/0000_00_00_000000_create_schools_table.php
+++ database/migrations/0000_00_00_000000_create_schools_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
             $table->string('website')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
